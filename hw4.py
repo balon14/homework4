40a7e1cdba38b8c7969e9f7a588474edc5f303df
balon14 <117996775+balon14@users.noreply.github.com>
2023-01-11 17:51:58 +0300

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-from transformers import pipeline 
+from transformers import pipeline
 from pydantic import BaseModel
-# Создаем класс для возможности передачи текста 
+
 class Item(BaseModel):
     text: str
+
 app = FastAPI()
-# Создаем модель 
-classifier = pipeline(model="SkolkovoInstitute/russian_toxicity_classifier")
-@app.get('/')
+classifier = pipeline("sentiment-analysis")
+
+@app.get("/")
 def root():
-    return {'message': 'Hello World'}
-# Функция для определения токсичных комментариев и передачи теска 
+    return {"message": "Hello World"}
+
 @app.post("/predict/")
 def predict(item: Item):
-    """Анализ токсичных комменатиев"""
-    return classifier(item.text )[0]
+    return classifier(item.text)[0]
